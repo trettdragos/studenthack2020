eb0cb74109fd7eec069f902bcd85b0cb2f29b291
dave <dave@pop-os.localdomain>
2020-04-25 18:29:14 +0300

    it's a starting point

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	// "fmt"
+	// "os"
+	// "regexp"
+	// htgotts "github.com/hegedustibor/htgo-tts"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/jzelinskie/geddit"
+)
+
+type joke struct {
+	Title       string `json:"Title"`
+	Description string `json:"Description"`
+}
+
+var jokes chan joke
+
+// func toSpeech(s geddit.Submission) {
+// 	reg, _ := regexp.Compile("[^a-zA-Z0-9'.]+")
+// 	speech := htgotts.Speech{Folder: "lines", Language: "en"}
+// 	line := reg.ReplaceAllString(s.Title+ s.Selftext," ")
+// 	fmt.Println(" got post ->", line)
+// 	speech.Speak(line[:200])
+// 	// filename := "lines/joke.mp3"
+// 	// src := "lines/" + s.Title + s.Selftext
+// 	// os.Rename(src, filename)
+// }
+
+func updateJokes(surplus int) {
+	fmt.Println("sunt aci baaah baaah")
+	session, err := geddit.NewLoginSession(
+		"quarantine_joke_bot",
+		"Boredathome69",
+		"gedditAgent v1",
+	)
+	if err != nil {
+		fmt.Println("nu mere contu baaah")
+	}
+	subOpts := geddit.ListingOptions{
+		Limit: surplus,
+	}
+	submissions, err := session.SubredditSubmissions("jokes", geddit.HotSubmissions, subOpts)
+	if err != nil {
+		fmt.Println("nu mer glumele baaah")
+	}
+	for i := 0; i < surplus; i++ {
+		latest := joke{Title: submissions[i].Title, Description: submissions[i].Selftext}
+		jokes <- latest
+		fmt.Println(latest.Title)
+	}
+	session.Clear()
+}
+
+func getJoke(w http.ResponseWriter, r *http.Request) {
+
+	data := <-jokes
+	fmt.Println(data.Title)
+	json.NewEncoder(w).Encode(data)
+}
+
+func main() {
+	updateJokes(1)
+	// router := mux.NewRouter().StrictSlash(true)
+	// router.HandleFunc("/", getJoke)
+	// log.Fatal(http.ListenAndServe(":8080", router))
+
+}
