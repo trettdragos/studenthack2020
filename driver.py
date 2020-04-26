9809588fb4c061e97a241c5c6a1e351032e6a2a2
trett <trettdragos@gmail.com>
2020-04-26 10:40:01 +0100

    Auto stash before merge of "master" and "origin/master"

--- driver.py
+++ driver.py
@@ -5,32 +5,31 @@ import time
 
 URL_JOKE = "http://138.197.73.249:80/joke"
 URL_COMM = "http://138.197.73.249:80/robot/getcommand"
-port = '/dev/ttyACM0'
-ard = Serial(port,9600,timeout=5)
+#port = '/dev/ttyACM0'
+#ard = Serial(port,9600,timeout=5)
 
 def say_joke():	
 	r = requests.get(url=URL_JOKE)
 	data = r.json()
 	print(data)
-	os.system('espeak -w audio.wav -ven-us+f4 -s170 "'+data['Title']+ ' ' + data['Description'] +'"')
-	os.system('owxplayer -o local --vol -1500 audio.wav')
-#	os.system('espeak -ven-us+f4 -s170 "'+data['Title']+'"')
-#	os.system('espeak -ven-us+f4 -s170 "'+data['Description']+'"')
+	os.system('espeak -w audio.wav -ven-us+f4 -s150 "'+data['Title']+'"'+data['Description']+'"')
+	os.system('omxplayer -o local --vol -1500 audio.wav')
 
 def relay_command(command):
 	print("send command")
 	ard.write(command.encode())
-	# time.sleep(1)
 
 while(1):
-	# relay_command("w")
 	r = requests.get(url=URL_COMM)
 	data = r.json()
 	print(data)
-	if(data == None):
+	if(data['Type'] == "null"):
 		print("no response")
 	else:
 		if(data['Type'] == "joke"):
 			say_joke();
 		if(data['Type'] == "move"):
 			relay_command(data['Direction'])
+		if(data['Type'] == "strange")
+			os.system('espeak -w audio.wav -ven-us+f4 -s150 "'+data['Direction']+'"')
+			os.system('omxplayer -o local --vol -1500 audio.wav')
